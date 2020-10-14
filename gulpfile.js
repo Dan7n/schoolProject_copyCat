cbe0ef15d9c8958f09b98965f90783781a06dd26
Dan7n <62748054+Dan7n@users.noreply.github.com>
2020-10-14 08:20:10 +0200

    Almost done
    Website behaves like it should, only thing left doing is getting the gulp file to work

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,24 @@
-let gulp = require('gulp');
-let scss = require('gulp-sass');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
 let csso = require('gulp-csso');
-let { watch } = require('gulp')
 
-watch(['Styles/*.scss', '!css/main.css'], function(cb) {
-    cb()
-});
+function style() {
+    return gulp.src('./Styles/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./Styles/main.css/main.css'))
+        .pipe(browserSync.stream());
+}
 
-gulp.task('default', function() {
-    return gulp.src('Styles/*scss')
-        .pipe(scss())
-        .pipe(gulp.dest('css'))
-        .pipe(csso())
-        .pipe(gulp.dest('css'));
+function watch() {
+    browserSync.init({
+        server: true,
+        baseDir: './',
 
-});
+    });
+}
+gulp.watch('./Styles/*.scss', style);
+gulp.watch('./index.html').on('change', browserSync.reload);
+
+exports.style = style;
+exports.watch = watch;
